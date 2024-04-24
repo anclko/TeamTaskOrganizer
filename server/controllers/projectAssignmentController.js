412c908971950b1a21c9aa4c0fbd9d986e019192
anclko <ackoch@hotmail.com>
2024-04-24 09:45:46 +0200

    used async handler for async functions in controll

--- server/controllers/projectAssignmentController.js
+++ server/controllers/projectAssignmentController.js
@@ -4,15 +4,10 @@ import { ProjectAssignment } from "../models/projectAssignmentModel.js";
 //@desc get all project assignments
 //@route Get api/project_assignments
 //@access public
-// This controller gets a list of project assignments along with employee full names and project names
-const getProjectAssignments = async (req,res) => {
-    try{
-        const proj = await ProjectAssignment.find()
-        res.status(200).json({proj})
-    }catch(err){
-        res.status(500).json({ error: err.message })
-    }
-}
+const getProjectAssignments = asyncHandler(async (req, res) => {
+    const proj = await ProjectAssignment.find();
+    res.status(200).json({ proj });
+});
 
 
 //@desc create new project assignment
@@ -21,13 +16,12 @@ const getProjectAssignments = async (req,res) => {
 const postProjectAssignment = asyncHandler(async (req, res) => {
     const { employee_id, project_code, start_date } = req.body;
 
-    // Check required fields
+    //check fields
     if (!employee_id || !project_code || !start_date) {
         res.status(400);
         throw new Error("All details are needed to create an assignment");
     }
 
-    // Attempt to create the project assignment
     try {
         const assignment = await ProjectAssignment.create({
             employee_id, 
@@ -35,7 +29,7 @@ const postProjectAssignment = asyncHandler(async (req, res) => {
             start_date
         });
 
-        // Send the created assignment as a response
+        //send info as response
         res.status(201).json(assignment);
     } catch (error) {
         res.status(500).json({ error: error.message });
